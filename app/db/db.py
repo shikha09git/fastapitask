5b4869c99249c7ea957b50e29f4461522d303024
shikha09git <shikha09jha@gmail.com>
2025-07-21 00:10:48 +0530

    mong db url changes

--- app/db/db.py
+++ app/db/db.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = "mongodb+srv://admin:admin@cluster0.0o5m4ap.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongo_client = AsyncIOMotorClient(MONGO_URI)
 mongo_db = mongo_client["FastApi_crud"]
 collection_product=mongo_db["Products"]
